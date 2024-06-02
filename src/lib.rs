d8c46c6697b0f59582e718184a68ae7381f09bcf
Matt Kulukundis <matt.fowles@gmail.com>
2024-06-02 17:07:28 -0400

    Start implementing patience diff
    - define the data types involved
    - implement the leading/trailing scan portion
    - punt on the hard part

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
-// 1. Match the first lines of both if they're identical, then match the second, third, etc. until
-//    a pair doesn't match.
-// 2. Match the last lines of both if they're identical, then match the next to last, second to
-//    last, etc. until a pair doesn't match.
-// 3. Find all lines which occur exactly once on both sides, then do longest common subsequence on
-//    those lines, matching them up.
-// 4. Do steps 1-2 on each section between matched lines.
-
 #![allow(unused)] // TODO(kfm): remove this
+
+use std::cmp;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 enum DiffItem {
@@ -23,8 +17,63 @@ enum DiffItem {
   },
 }
 
-fn diff(lhs: &Vec<&str>, rhs: &Vec<&str>) -> Vec<DiffItem> {
-  return vec![];
+fn leading_match_len(lhs: &[&str], rhs: &[&str]) -> usize {
+  for n in 0..cmp::min(lhs.len(), rhs.len()) {
+    if lhs[n] != rhs[n] {
+      return n;
+    }
+  }
+  cmp::min(lhs.len(), rhs.len())
+}
+
+fn trailing_match_len(lhs: &[&str], rhs: &[&str]) -> usize {
+  for n in 0..cmp::min(lhs.len(), rhs.len()) {
+    if lhs[lhs.len() - 1 - n] != rhs[rhs.len() - 1 - n] {
+      return n;
+    }
+  }
+  cmp::min(lhs.len(), rhs.len())
+}
+
+// Patience diff algorithm
+//
+// 1. Match the first lines of both if they're identical, then match the second,
+//    third, etc. until a pair doesn't match.
+// 2. Match the last lines of both if they're identical, then match the next to
+//    last, second to last, etc. until a pair doesn't match.
+// 3. Find all lines which occur exactly once on both sides, then do longest
+//    common subsequence on those lines, matching them up.
+// 4. Do steps 1-2 on each section between matched lines.
+fn diff(lhs: &[&str], rhs: &[&str]) -> Vec<DiffItem> {
+  let mut r = Vec::new();
+  let leading = leading_match_len(lhs, rhs);
+  if leading != 0 {
+    r.push(DiffItem::Match { lhs: 0, rhs: 0, len: leading });
+  }
+
+  if leading == lhs.len() && leading == rhs.len() {
+    return r;
+  }
+
+  let trailing = trailing_match_len(lhs, rhs);
+
+  // TODO(kfm): step 3
+  r.push(DiffItem::Mutation {
+    lhs_pos: leading,
+    lhs_len: lhs.len() - leading - trailing,
+    rhs_pos: leading,
+    rhs_len: rhs.len() - leading - trailing,
+  });
+
+  if trailing != 0 {
+    r.push(DiffItem::Match {
+      lhs: lhs.len() - trailing,
+      rhs: rhs.len() - trailing,
+      len: trailing,
+    });
+  }
+
+  r
 }
 
 #[cfg(test)]
@@ -32,7 +81,20 @@ mod tests {
   use super::*;
 
   #[test]
-  fn basic() {
+  fn empty() {
+    assert_eq!(diff(&vec![], &vec![]), vec![]);
+  }
+
+  #[test]
+  fn identical() {
+    assert_eq!(
+      diff(&vec!["a", "b", "c"], &vec!["a", "b", "c"]),
+      vec![DiffItem::Match { lhs: 0, rhs: 0, len: 3 }]
+    );
+  }
+
+  #[test]
+  fn different() {
     assert_eq!(
       diff(&vec!["a", "b", "c"], &vec!["a", "c"]),
       vec![
