e24362fa65d7d338e296230691c9583130f17de6
Matt Kulukundis <matt.fowles@gmail.com>
2024-06-03 10:11:42 -0400

    Add match_unique_lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(unused)] // TODO(kfm): remove this
 
-use std::cmp;
+use std::collections::HashMap;
 use std::iter::zip;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
@@ -69,17 +69,61 @@ fn trailing_match_len(lhs: &[&str], rhs: &[&str]) -> usize {
     .count()
 }
 
+fn partition(lhs: &[&str], rhs: &[&str]) -> Vec<DiffItem> {
+  let mut r = vec![];
+  if lhs.is_empty() && rhs.is_empty() {
+    // nothing
+  } else if (lhs.is_empty()) {
+    r.push(DiffItem::Mutation {
+      lhs_pos: 0,
+      lhs_len: 0,
+      rhs_pos: 0,
+      rhs_len: rhs.len(),
+    });
+  } else if (rhs.is_empty()) {
+    r.push(DiffItem::Mutation {
+      lhs_pos: 0,
+      lhs_len: lhs.len(),
+      rhs_pos: 0,
+      rhs_len: 0,
+    });
+  } else {
+    let matched = match_unique_lines(lhs, rhs);
+
+  }
+  r
+}
+
+fn match_unique_lines(lhs: &[&str], rhs: &[&str]) -> Vec<(usize, usize)> {
+  let mut m = HashMap::<&str, (Vec<usize>, Vec<usize>)>::new();
+  for (i, l) in lhs.iter().enumerate() {
+    m.entry(l).or_default().0.push(i);
+  }
+  for (i, r) in rhs.iter().enumerate() {
+    m.entry(r).or_default().1.push(i);
+  }
+
+  let mut v: Vec<(usize, usize)> = m
+    .iter()
+    .map(|(_, v)| v)
+    .filter(|(l, r)| l.len() == 1 && r.len() == 1)
+    .map(|(l, r)| (l[0], r[0]))
+    .collect();
+  v.sort();
+  v
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
 
   #[test]
-  fn empty() {
+  fn diff_empty() {
     assert_eq!(diff(&vec![], &vec![]), vec![]);
   }
 
   #[test]
-  fn identical() {
+  fn diff_eq() {
     assert_eq!(
       diff(&vec!["a", "b", "c"], &vec!["a", "b", "c"]),
       vec![DiffItem::Match { lhs: 0, rhs: 0, len: 3 }]
@@ -87,7 +131,7 @@ mod tests {
   }
 
   #[test]
-  fn different() {
+  fn diff_ne() {
     assert_eq!(
       diff(&vec!["a", "b", "c"], &vec!["a", "c"]),
       vec![
@@ -100,6 +144,17 @@ mod tests {
         },
         DiffItem::Match { lhs: 2, rhs: 1, len: 1 },
       ]
+    );
+  }
+
+  #[test]
+  fn match_unique_lines_basic() {
+    assert_eq!(
+      match_unique_lines(
+        &vec!["a", "b", "c", "d", "e", "d",],
+        &vec!["a", "c", "d", "e"]
+      ),
+      vec![(0, 0), (2, 1), (4, 3)]
     );
   }
 }
