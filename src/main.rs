8a595cd9e19e53f41b9f55265d9f7eab1f4ce674
Matt Kulukundis <matt.fowles@gmail.com>
2024-06-04 12:10:49 -0400

    add main with clap args

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version)]
+#[command(about = "Diff files using patience algorithm")]
+struct Args {
+  /// Path to old file or `-` for stdin.
+  #[clap(name = "OLD_FILE")]
+  lhs: String,
+
+  /// Path to new file or `-` for stdin.
+  #[clap(name = "NEW_FILE")]
+  rhs: String,
+}
+
+fn main() {
+  let _ = Args::parse();
+
+  println!("Hello, world!");
+}
