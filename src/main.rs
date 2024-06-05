26d0c3fdf6a90857c137c06d2230d8ed2d2b6d73
Matt Kulukundis <matt.fowles@gmail.com>
2024-06-05 17:07:30 -0400

    Early progress formatting output

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-pub mod files;
+#![allow(unused)] // TODO(kfm): remove this
 
 use anstream::println;
 use clap::Parser;
-use files::Contents;
-use owo_colors::OwoColorize;
+use owo_colors::{OwoColorize, Style, Styled};
 use std::error::Error;
 use std::path::PathBuf;
 
@@ -24,19 +23,96 @@ struct Args {
   context: usize,
 }
 
-fn type_suffix(f: &Contents) -> &'static str {
+mod files;
+
+fn type_suffix(f: &files::Contents) -> &'static str {
   match f {
-    Contents::Binary(_) => " (binary)",
+    files::Contents::Binary(_) => " (binary)",
     _ => "",
   }
 }
 
-fn print_diff(lhs: &str, rhs: &str) {
-  let d = pratdiff::diff_lines(lhs, rhs);
-  println!("{:?}", d);
+fn offset_range(lhs: (usize, usize), rhs: (usize, usize)) -> String {
+  format!("@@ -{},{} +{},{} @@", lhs.0 + 1, lhs.1, rhs.0 + 1, rhs.1)
+}
+
+use pratdiff::DiffItem;
+use pratdiff::DiffItem::*;
+
+fn lines(
+  lhs: &[&str],
+  rhs: &[&str],
+  context: usize,
+  diff: &DiffItem,
+) -> Vec<(String, Style)> {
+  let header: Style = Style::new().cyan();
+  let both: Style = Style::new().default_color();
+  let old: Style = Style::new().red();
+  let new: Style = Style::new().green();
+
+  match *diff {
+    Mutation { lhs_pos, lhs_len, rhs_pos, rhs_len } => {
+      let mut r = Vec::new();
+      for line in &lhs[lhs_pos..lhs_pos + lhs_len] {
+        r.push((format!("-{}", line), old));
+      }
+      for line in &rhs[rhs_pos..rhs_pos + rhs_len] {
+        r.push((format!("+{}", line), new));
+      }
+      r
+    }
+    Match { lhs: lhs_pos, rhs: rhs_pos, len } => {
+      let mut r = Vec::new();
+      if len > 2 * context {
+        for line in &lhs[lhs_pos..lhs_pos + context] {
+          r.push((format!(" {}", line), both));
+        }
+        r.push((
+          format!(
+            "@@ -{},{} +{},{}  @@",
+            lhs_pos + context,
+            0,
+            rhs_pos + context,
+            0
+          ),
+          header,
+        ));
+        for line in &lhs[lhs_pos + len - context..lhs_pos + len] {
+          r.push((format!(" {}", line), both));
+        }
+      } else {
+        for line in &lhs[lhs_pos..lhs_pos + len] {
+          r.push((format!(" {}", line), both));
+        }
+      }
+      r
+    }
+  }
+}
+
+fn print_diff(context: usize, lhs_all: &str, rhs_all: &str) {
+  let header: Style = Style::new().cyan();
+  let old: Style = Style::new().red();
+  let new: Style = Style::new().green();
+
+  let lhs: Vec<_> = lhs_all.lines().collect();
+  let rhs: Vec<_> = rhs_all.lines().collect();
+  let diffs = pratdiff::diff(&lhs, &rhs);
+
+  let mut lhs_printed: usize = 0;
+  let mut rhs_printed: usize = 0;
+  for diff in diffs {
+    for (line, style) in lines(&lhs, &rhs, context, &diff) {
+      println!("{}", line.style(style));
+    }
+  }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+  let header: Style = Style::new().bold().white();
+  let old: Style = Style::new().red();
+  let new: Style = Style::new().green();
+
   let args = Args::parse();
 
   let lhs = files::read(&args.lhs)?;
@@ -46,17 +122,17 @@ fn main() -> Result<(), Box<dyn Error>> {
   }
 
   match (&lhs, &rhs) {
-    (Contents::Text(l), Contents::Text(r)) => {
-      println!("{} {}", "---".red(), args.lhs.display().bold().white());
-      println!("{} {}", "+++".green(), args.rhs.display().bold().white());
-      print_diff(l, r);
+    (files::Contents::Text(l), files::Contents::Text(r)) => {
+      println!("{} {}", "---".style(old), args.lhs.display().style(header));
+      println!("{} {}", "+++".style(new), args.rhs.display().style(header));
+      print_diff(args.context, l, r);
     }
     _ => {
       println!(
         "Files {}{} and {}{} differ",
-        args.lhs.display().red(),
+        args.lhs.display().style(old),
         type_suffix(&lhs),
-        args.rhs.display().green(),
+        args.rhs.display().style(new),
         type_suffix(&rhs),
       );
     }
