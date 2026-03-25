ab28d22097d22fd83e4775e36705eebcab9370e7
Matt Kulukundis <matt.fowles@gmail.com>
2026-03-25 03:34:37 -0400

    update dependencies

--- build.rs
+++ build.rs
@@ -1,9 +1,12 @@
-use vergen::EmitBuilder;
+use vergen::{BuildBuilder, Emitter};
+use vergen_gitcl::GitclBuilder;
 
-fn main() {
-  EmitBuilder::builder()
-    .build_date()
-    .git_describe(true, false, None)
-    .emit()
-    .unwrap();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let build = BuildBuilder::default().build_date(true).build()?;
+  let gitcl = GitclBuilder::default().describe(true, false, None).build()?;
+  Emitter::default()
+    .add_instructions(&build)?
+    .add_instructions(&gitcl)?
+    .emit()?;
+  Ok(())
 }
